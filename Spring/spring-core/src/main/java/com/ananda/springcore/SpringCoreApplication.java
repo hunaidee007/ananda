a7bb59ce7e25d03ea6c71ac2d9404b4fff771c93
Hunaid Husain <hunaidee007@gmail.com>
2023-08-01 14:43:37 +0100

    Added a print

--- Spring/spring-core/src/main/java/com/ananda/springcore/SpringCoreApplication.java
+++ Spring/spring-core/src/main/java/com/ananda/springcore/SpringCoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Spring Context...");
 		SpringApplication.run(SpringCoreApplication.class, args);
 	}
 
